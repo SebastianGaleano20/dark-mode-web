d94253121d0b05c47876738338f8aae2f96992a5
Sebastian Galeano <sebascarp9@gmail.com>
2024-08-04 11:17:06 -0300

    zustand config

---
+++ src/stores/useTheme.js
@@ -0,0 +1,24 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+import { THEME_TYPES } from "../constants";
+
+const { THEME_LIGHT, THEME_DARK } = THEME_TYPES;
+const toggleTheme = (currentTheme) =>
+  currentTheme === THEME_LIGHT ? THEME_DARK : THEME_LIGHT;
+
+const useThemeStore = create(
+  persist(
+    (set) => ({
+      theme: THEME_LIGHT,
+      toggleTheme: () =>
+        set((state) => ({
+            theme: toggleTheme(state.getState().theme), 
+        })),
+    }),
+    {
+      name: "theme",
+    }
+  )
+);
+
+export default useThemeStore;
